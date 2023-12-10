9c157b3d89d65f1fa53fab74d329460e6320ea8a
putilin.lev <vash-agent48@yandex.ru>
2023-12-10 22:33:53 +0300

    diplom

--- cmd/gophermart/api/orders_func.go
+++ cmd/gophermart/api/orders_func.go
@@ -8,12 +8,12 @@ import (
 	"go.uber.org/zap"
 )
 
-func (app *App) SaveOrderNumber(orderNumber, userEmail string) error {
+func (mc *App) SaveOrderNumber(orderNumber, userEmail string) error {
 	log := logger.GetLogger()
 
 	// Проверка на уникальность номера заказа перед вставкой
 	var count int
-	err := app.DB.QueryRow("SELECT COUNT(*) FROM orders WHERE order_number = $1", orderNumber).Scan(&count)
+	err := mc.DB.QueryRow("SELECT COUNT(*) FROM orders WHERE order_number = $1", orderNumber).Scan(&count)
 	if err != nil {
 		log.Error("Ошибка при проверке уникальности номера заказа", zap.Error(err))
 		return err
@@ -25,7 +25,7 @@ func (app *App) SaveOrderNumber(orderNumber, userEmail string) error {
 	// Запрос на вставку номера заказа и адреса электронной почты в базу данных
 	query := "INSERT INTO orders (order_number, user_email, status, created_at) VALUES ($1, $2, 'NEW', $3)"
 	createdAt := time.Now() // Используем текущее время
-	_, err = app.DB.Exec(query, orderNumber, userEmail, createdAt)
+	_, err = mc.DB.Exec(query, orderNumber, userEmail, createdAt)
 	if err != nil {
 		log.Error("Ошибка при сохранении номера заказа в базе данных", zap.Error(err))
 		return err
